f8de2afe6b6af0900bc4e68fe6f06e5084cbf1f2
Marco00 <marcopistollato.00@gmail.com>
2017-10-05 17:40:50 +0200

    esercizio cambio stati thread

---
+++ 170929_Stato/TestDataDownload.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testdatadownload;
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+/**
+ *
+ * @author Marco
+ */
+public class TestDataDownload {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DataDownloader ddl = new DataDownloader();
+        Thread td = new Thread(ddl,"DataDownloaderThread");
+        Thread tn = new Network(td,"NetworkConnectionThread");
+        Thread osserva = new Osservatore(td, tn);
+        osserva.start();
+        tn.start();
+        td.start();
+        try {
+            TimeUnit.SECONDS.sleep(8);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        osserva.interrupt();
+    } 
+}
+
+
+class DataDownloader implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.printf("Connecting: %s\n", new Date());
+        try {
+            TimeUnit.SECONDS.sleep(4);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("Download finished: %s\n", new Date());
+    }
+}
+
+
+class Network extends Thread {
+    private Thread td;  // the downloadr
+
+    Network(Thread td, String name){
+        super(name);
+        this.td = td;
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("Network connected: %s\n", new Date());
+        try {
+            td.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("Network not connected: %s\n", new Date());
+    }
+}
+
+
+class Osservatore extends Thread{
+    private Thread td, tn;
+
+    public Osservatore(Thread td, Thread tn){
+        this.td = td;
+        this.tn = tn;
+    }
+
+    @Override
+    public void run(){
+        Thread.State tdStato = td.getState();
+        Thread.State tnStato = tn.getState();
+        System.out.println(td.getName()+"; stato: "+tdStato);
+        System.out.println(tn.getName()+"; stato: "+tnStato);
+        while(true){
+            if(!td.getState().equals(tdStato)) {
+                tdStato = td.getState();
+                System.out.println(td.getName()+"; stato: "+tdStato);
+            }
+            if(!tn.getState().equals(tnStato)){
+                tnStato = tn.getState();
+                System.out.println(tn.getName()+"; stato: "+tnStato);
+            }
+            try{
+                TimeUnit.MILLISECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                System.out.println("Osservazione degli stati completata");
+                break;
+            }
+        }
+    }
+}
