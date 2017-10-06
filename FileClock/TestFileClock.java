6451b9aa5f94a97c0a9d5b24c325b14395e78c95
Romano Giacomo <giacomoromano15@gmail.com>
2017-10-06 18:03:02 +0200

    Update TestFileClock.java

--- FileClock/TestFileClock.java
+++ FileClock/TestFileClock.java
@@ -3,13 +3,10 @@ import java.util.Random;
 public class TestFileClock{
     public static void main(String[] args){
         Thread clocks[] = new Thread[10];
-        System.out.println("clocks initialized");
         for(int i=0;i<10;i++){
             clocks[i] = new Thread(new FileClock());
             clocks[i].setName("Clock "+ i);
-            System.out.println("... status of " + clocks[i].getName() + ": " + clocks[i].getState());
         }
-        System.out.println("clocks starting");
         for(int i=0;i<10;i++){
             running(clocks, i);
         }
@@ -22,7 +19,8 @@ public class TestFileClock{
         t[i].start();
         try{
             t[whichOne].sleep(howMuch);
-            System.out.println("... " + t[i].getName() + " interrupted " + t[whichOne].getName() + " for " + howMuch/1000 + " seconds");
+            System.out.println("... " + t[i].getName() + " interrupted " + t[whichOne].getName() + " for " 
+            + howMuch/1000 + " seconds\t"+t[whichOne].getName()+" --> TIMED_WAITING");
         }catch(InterruptedException ex){}
     }
 }
