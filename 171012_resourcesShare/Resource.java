d61789a6b2692af78059957ed56e2ccdbc341fc1
Ospite <guest-8nr1zp@lap2-ws15.cz.lan>
2017-10-12 09:03:28 +0200

    aggiunto esercizio risorse condivise

---
+++ 171012_resourcesShare/Resource.java
@@ -0,0 +1,18 @@
+import java.util.concurrent.Semaphore;
+
+public class Resource{
+	public Semaphore semaphore;
+	private int info;
+	
+	public Resource(int info){
+		this.info = info;
+		semaphore = new Semaphore(1, true);
+	}
+	
+		public void setInfo(int arg){
+		this.info = arg;
+	}
+	public int getInfo(){
+		return info;
+	}
+}
