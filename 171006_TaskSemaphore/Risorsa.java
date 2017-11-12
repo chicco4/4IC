c52c747652cd066ab1763470a57d9d8031a99b1d
Giack30 <g.momo.2000@gmail.com>
2017-11-12 17:09:31 +0100

    Update Risorsa.java

--- 171006_TaskSemaphore/Risorsa.java
+++ 171006_TaskSemaphore/Risorsa.java
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
+
 import java.util.concurrent.Semaphore;
 
 public class Risorsa{
 	private Semaphore semaphore;
-=======
 package test;
 import java.util.concurrent.Semaphore;
 
@@ -14,7 +13,6 @@ import java.util.concurrent.Semaphore;
 
 public class Risorsa{
 	Semaphore semaphore;
->>>>>>> ccc0d11c21639228754928ce12a4da03a5369b4f
 	public String name;
 	
 	public Risorsa(String name){
